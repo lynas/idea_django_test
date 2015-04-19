3251dd987951df78cfd5c60bffccb7d2e999353c
sazzad <sazzad@ipvision-inc.com>
2015-04-19 18:18:10 +0600

    user authentication done

--- djangoTest/urls.py
+++ djangoTest/urls.py
@@ -7,5 +7,11 @@ urlpatterns = [
     # url(r'^blog/', include('blog.urls')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'', include('mysite.urls')),
+
+    url(r'^auth/login/$', 'mysite.views.login'),
+    url(r'^auth/logout/$', 'mysite.views.logout'),
+    url(r'^auth/invalid/$', 'mysite.views.invalid_login'),
+    url(r'^auth/authenticate/$', 'mysite.views.authenticate'),
+    url(r'^auth/loggedin/$', 'mysite.views.loggedin'),
 ]
 
--- mysite/views.py
+++ mysite/views.py
@@ -1,6 +1,8 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 import subprocess
 from mysite.models import Person
+from django.contrib import auth
 # from mysite.models import UsersOfIpv
 
 
@@ -36,3 +38,33 @@ def form_submit_page(request):
 
     return render(request, "form_submit_page.html", context)
 
+
+def authenticate(request):
+    username = request.POST.get('username', '')
+    password = request.POST.get('password', '')
+
+    user = auth.authenticate(username=username, password=password)
+    if user is not None:
+        auth.login(request, user)
+        return HttpResponseRedirect('/auth/loggedin')
+    else:
+        return HttpResponseRedirect('/auth/invalid/')
+
+
+def loggedin(request):
+    context = {
+        'username': request.user.username
+    }
+    return render(request, 'loggedin.html', context)
+
+
+def login(request):
+    return render(request, 'login.html', context={})
+
+
+def invalid_login(request):
+    return render(request, 'invalid_login.html', context={})
+
+
+def logout(request):
+    return render(request, 'logout.html', context={})
